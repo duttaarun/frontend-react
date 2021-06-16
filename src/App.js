c9829960f8b5b1844e0dea3fead157225450b0a6
duttaarun <duttaarun2015@gmail.com>
2021-06-16 19:49:02 +0530

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import "./App.css";
+import Loader from "react-loader-spinner";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
       isLoaded: false,
       chartData: {},
       chinaBorderData: undefined,
+      loading: true
     };
   }
 
@@ -78,87 +80,108 @@ export default class App extends React.Component {
             });
           }
         ),
+      setTimeout(() => {
+        this.setState({ loading: false })
+      }, 5000)
     ]);
+
   }
 
   render() {
-    return (
-      <div className="App">
-        <div className="header">
-          <h1 className="title">Demo Application</h1>
-          <div className="links"></div>
-        </div>
 
-        <button
-          class="button button1"
-          onClick={() =>
-            window.open(
-              `${process.env.REACT_APP_API_URL}/generatePdfForDashboard`,
-              "_blank"
-            )
-          }
-        >
-          Generate PDF
-        </button>
-
-        <div className="barchart">
-          <Bar
-            width={50}
-            height={20}
-            data={this.state.chartData}
-            options={{
-              maintainAspectRatio: true,
-              title: {
-                display: true,
-                text: "Countries with most border",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            }}
+    if (this.state.loading) {
+      return (
+        <div className="App loader">
+          <Loader
+            type="Puff"
+            color="green"
+            height={100}
+            width={100}
+            timeout={5000}
           />
         </div>
+      )
+    }
+    else {
+      return (
+        <div className="App">
 
-        <h2>Countries bordered by China</h2>
-        <div className="section">
-          <table>
-            <thead>
-              <tr>
-                <td>Name</td>
-                <td>Region</td>
-                <td>Area</td>
-                <td>Population</td>
-                <td>Flag</td>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.chinaBorderData &&
-                this.state.chinaBorderData.map((listValue, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{listValue.countryName}</td>
-                      <td>{listValue.region}</td>
-                      <td>{listValue.area}</td>
-                      <td>{listValue.population}</td>
-                      <td>
-                        {" "}
-                        <img
-                          src={listValue.flagSvgUrl}
-                          width="35px"
-                          height="25px"
-                          alt="Not Found"
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
+          <div className="header">
+            <h1 className="title">Demo Application</h1>
+            <div className="links"></div>
+          </div>
+
+          <button
+            class="button button1"
+            onClick={() =>
+              window.open(
+                `${process.env.REACT_APP_API_URL}/generatePdfForDashboard`,
+                "_blank"
+              )
+            }
+          >
+            Generate PDF
+          </button>
+
+          <div className="barchart">
+            <Bar
+              width={50}
+              height={20}
+              data={this.state.chartData}
+              options={{
+                maintainAspectRatio: true,
+                title: {
+                  display: true,
+                  text: "Countries with most border",
+                  fontSize: 20,
+                },
+                legend: {
+                  display: true,
+                  position: "right",
+                },
+              }}
+            />
+          </div>
+
+          <h2>Countries bordered by China</h2>
+          <div className="section">
+            <table>
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>Region</td>
+                  <td>Area</td>
+                  <td>Population</td>
+                  <td>Flag</td>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.chinaBorderData &&
+                  this.state.chinaBorderData.map((listValue, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{listValue.countryName}</td>
+                        <td>{listValue.region}</td>
+                        <td>{listValue.area}</td>
+                        <td>{listValue.population}</td>
+                        <td>
+                          {" "}
+                          <img
+                            src={listValue.flagSvgUrl}
+                            width="35px"
+                            height="25px"
+                            alt="Not Found"
+                          />
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 
   openInNewTab = (url) => {
